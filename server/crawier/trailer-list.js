8548411b3d9a79cbe5c683d9f01988e63815b238
shijiandatou <m15531178940@163.com>
2018-05-22 23:21:46 +0800

    add koa2

---
+++ server/crawier/trailer-list.js
@@ -0,0 +1,59 @@
+
+const url = `https://movie.douban.com/tag/#/?sort=R&rang=6,10&`;
+
+const puppeteer = require('puppeteer');
+
+const sleep = time => new Promise(resolve =>{
+    setTimeout(resolve,time);
+})
+;(async ()=>{
+    console.log('现在开始了');
+
+    const browser  = await puppeteer.launch({
+        args:['--no-sandbox'],
+        dumpio:false
+    })
+ //   const browser = await browserPromise;
+    const page = await browser.newPage();
+    await page.goto(url,{
+        waitUntil:'networkidle2'
+    })
+
+    await sleep(3000);
+
+    await page.waitForSelector('.more');
+
+    for(let i=0; i<1; i++){
+        await sleep(3000);
+
+        await page.click('.more')
+    }
+
+    const  resule = await page.evaluate(() => {
+        var $ = window.$;
+        var items = $('.list-wp a');
+        var links = [];
+
+        if(items.length >=1){
+            items.each((index,item) =>{
+                let it = $(item);
+                let doubanId = it.find('div').data('id');
+                let tittle = it.find('.title').text();
+                let rate = Number(it.find('.rate').text());
+                let poster = it.find('img').attr('src').replace('s_ratio','l_ratio');
+
+                links.push({
+                    doubanId,
+                    tittle,
+                    rate,
+                    poster
+                })
+            })
+        }
+        return links;
+    })
+
+    browser.close();
+
+    console.log(resule);
+})()
